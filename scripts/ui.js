6ea30b050a04faa63dad150322d22ffed8e22872
Giboney <70110379+Giboney@users.noreply.github.com>
2026-06-01 20:18:24 -0700

    Popup abandoned, Bench Tabs started

--- scripts/ui.js
+++ scripts/ui.js
@@ -106,112 +106,162 @@ function createFieldCollapseBtn() {
     })
 }
 
-// create optimizer panel
-let optimizerPanel = $('<fieldset>', {id: 'optimizerPanel'})
-optimizerPanel.append($('<legend>', {text: 'Optimizer'}))
-$('.main-result-group').after(optimizerPanel)
-
-// fixing base calc panel positioning (it was off before)
-// calcPanels div is display flex
-let calcPanelsDiv = $('<div>', {id: 'calcPanels'})
-optimizerPanel.after(calcPanelsDiv)
-$('.panel').appendTo(calcPanelsDiv)
-
-//create bench window
-let benchDiv = $('<div>') // add id later if needed
-let benchTabs = $('<div>', {id: 'benchTabs'})
-let benchWindow = $('<div>', {id: 'benchWindow'})
-benchDiv.append([benchTabs, benchWindow])
-$('.move-result-subgroup:has(#resultHeaderL)').after(benchDiv)
-
-//move % damage to left side for p1
-for (let i = 1; i <= 4; i++) {
-    $(`#resultDamageL${i}`).prependTo($(`#resultDamageL${i}`).parent())
-}
-
-// fix rolls display
-$(".result-move").off('change')
-
-displayDamageHits = function(damage) {
-    // Fixed Damage
-	if (typeof damage === 'number') return "(" + damage.toString() + ")";
-	// Standard Damage
-	if (damage.length > 2 && typeof damage[0] === 'number')
-		return "(" + damage.join(', ') + ")";
-	// Fixed Parental Bond Damage
-	if (typeof damage[0] === 'number' && typeof damage[1] === 'number') {
-		return '1st Hit: (' + damage[0] + ')\n2nd Hit: (' + damage[1] + ")";
-	}
-	// Multihit Damage
-	var fullText = "";
-	for (var i = 1; i <= damage.length; i++) {
-		var txt = toOrdinal(i) + " Hit: (" + damage[i - 1].join(', ') + ")";
-		if (i < damage.length) txt += "\n";
-		fullText += txt;
-		// if (i % 2 == 1 && i < damage.length) fullText += "\n";
-	}
-    console.log(fullText)
-	return fullText;
-}
-
-$(".result-move").on('change', function () {
-	if (damageResults) {
-		var result = findDamageResult($(this));
-		if (result) {
-			var desc = result.fullDesc(notation, false);
-			if (desc.indexOf('--') === -1) desc += ' -- possibly the worst move ever';
-			$("#mainResult").text(desc);
-			var summary = displayDamageHits(result.damage);
-			var rest = "";
-			var newLine = summary.indexOf('\n');
-			if (newLine > -1) {
-				rest = summary.substring(newLine + 1);
-				summary = summary.substring(0, newLine);
-			}
-			$("#firstDmgValues").text("Possible damage amounts: " + summary);
-			if (rest !== "") $("#restDmgValues").text(rest);
-
-			if (rest.trim() === "") {
-				$("#firstDmgValues").css("display", "block");
-				$("#restDmgValues").text("");
-			} else {
-				$("#damageValues").removeAttr("open");
-				$("#firstDmgValues").css("display", "revert");
-			}
-		}
-	}
-});
-
-// fix/add copyable result/rolls
-let rolls = $('#damageValues')
-let rollsTooltip = $('<div>', {id: 'rollsTooltip', style: 'visibility: hidden', text: 'Copied'})
-rolls.attr('title', 'Right-click to copy.')
-rolls.after(rollsTooltip)
-rolls.on('contextmenu', function() { return false })
-rolls.on('mousedown', function(e) {
-    if (e.button === 2) {
-        rolls.one('mouseup', function() {
-            if (e.button === 2) {
-                let text = $('#firstDmgValues').text() + '\n' + $('#restDmgValues').text()
-                navigator.clipboard.writeText(text).then(function () {
-                    $('#rollsTooltip').css('visibility', 'visible')
-                    setTimeout(function () {
-                        $('#rollsTooltip').css('visibility', 'hidden')
-                    }, 1500);
-                })
-            }
+function benchTabBtn(name, position) {
+    position = position.toLowerCase()
+    let id = `benchTab${$('#benchTbas > input').length}`
+    return [
+        $('<input>', {
+            id: id,
+            class: 'bench-tab visually-hidden',
+            type: 'radio',
+            name: 'resultMove'
+        }),
+        $('<label>', {
+            class: 'btn btn-' + position,
+            for: id,
+            text: name,
+            title: 'Right click for options.'
         })
+    ]
+}
+
+function doPanels() {
+    // create optimizer panel
+    let optimizerPanel = $('<fieldset>', {id: 'optimizerPanel'})
+    optimizerPanel.append($('<legend>', {text: 'Optimizer'}))
+    $('.main-result-group').after(optimizerPanel)
+    // fixing base calc panel positioning (it was off before)
+    // calcPanels div is display flex
+    let calcPanelsDiv = $('<div>', {id: 'calcPanels'})
+    optimizerPanel.after(calcPanelsDiv)
+    $('.panel').appendTo(calcPanelsDiv)
+}
+
+function fixCalculationResults() {
+    $(".result-move").off('change')
+    // adjusted so that each hit has a newline, and parenthesis are included
+    displayDamageHits = function(damage) {
+        // Fixed Damage
+        if (typeof damage === 'number') return "(" + damage.toString() + ")";
+        // Standard Damage
+        if (damage.length > 2 && typeof damage[0] === 'number')
+            return "(" + damage.join(', ') + ")";
+        // Fixed Parental Bond Damage
+        if (typeof damage[0] === 'number' && typeof damage[1] === 'number') {
+            return '1st Hit: (' + damage[0] + ')\n2nd Hit: (' + damage[1] + ")";
+        }
+        // Multihit Damage
+        var fullText = "";
+        for (var i = 1; i <= damage.length; i++) {
+            var txt = toOrdinal(i) + " Hit: (" + damage[i - 1].join(', ') + ")";
+            if (i < damage.length) txt += "\n";
+            fullText += txt;
+            // if (i % 2 == 1 && i < damage.length) fullText += "\n";
+        }
+        return fullText;
     }
-})
+    $(".result-move").on('change', function () {
+        if (damageResults) {
+            var result = findDamageResult($(this));
+            if (result) {
+                var desc = result.fullDesc(notation, false);
+                if (desc.indexOf('--') === -1) desc += ' -- possibly the worst move ever';
+                $("#mainResult").text(desc);
+                var summary = displayDamageHits(result.damage);
+                var rest = "";
+                var newLine = summary.indexOf('\n');
+                if (newLine > -1) {
+                    rest = summary.substring(newLine + 1);
+                    summary = summary.substring(0, newLine);
+                }
+                $("#firstDmgValues").text("Possible damage amounts: " + summary);
+                if (rest !== "") $("#restDmgValues").text(rest);
+
+                if (rest.trim() === "") {
+                    $("#firstDmgValues").css("display", "block");
+                    $("#restDmgValues").text("");
+                } else {
+                    $("#damageValues").removeAttr("open");
+                    $("#firstDmgValues").css("display", "revert");
+                }
+            }
+        }
+    });
+    // fix/add copyable result/rolls
+    let rolls = $('#damageValues')
+    let rollsTooltip = $('<div>', {id: 'rollsTooltip', style: 'visibility: hidden', text: 'Copied'})
+    rolls.attr('title', 'Right-click to copy.')
+    rolls.after(rollsTooltip)
+    rolls.on('contextmenu', function() { return false })
+    rolls.on('mousedown', function(e) {
+        if (e.button === 2) {
+            rolls.one('mouseup', function() {
+                if (e.button === 2) {
+                    let text = $('#firstDmgValues').text() + '\n' + $('#restDmgValues').text()
+                    navigator.clipboard.writeText(text).then(function () {
+                        $('#rollsTooltip').css('visibility', 'visible')
+                        setTimeout(function () {
+                            $('#rollsTooltip').css('visibility', 'hidden')
+                        }, 1500);
+                    })
+                }
+            })
+        }
+    })
+}
+
+function createBenchBrowser() {
+    //create bench window
+    let benchDiv = $('<div>', {id: 'benchBrowser'}) // add id later if needed
+    let benchTabs = $('<div>', {id: 'benchTabs'})
+    let benchWindow = $('<div>', {id: 'benchWindow'})
+    benchDiv.append([benchTabs, benchWindow])
+    $('.move-result-subgroup:has(#resultHeaderL)').after(benchDiv)
+    //move % damage to left side for p1
+    for (let i = 1; i <= 4; i++) {
+        $(`#resultDamageL${i}`).prependTo($(`#resultDamageL${i}`).parent())
+    }
+
+    benchTabs.append(benchTabBtn('Benchmark 1', 'top'))
+
+    //add tab context menu
+    let benchTabOptions = $('<div>', {id: 'benchTabOptions', style: 'display: none'})
+    benchTabOptions.append([
+        $('<span>', {id: 'benchRename', text: 'Rename'}),
+        $('<span>', {id: 'benchDelete', text: 'Delete'})
+    ])
+    $('body').prepend(benchTabOptions)
+
+    benchTabs.on('contextmenu', '.bench-tab + .btn', function(e) {
+        e.preventDefault()
+        if (benchTabOptions.is(':hidden')) {
+            benchTabOptions.css({'left': e.pageX, 'top': e.pageY, 'display': 'flex'})
+        } else {
+            benchTabOptions.hide()
+        }
+    })
+
+    // clear context menu when clicking elsewhere
+    $(document).on('click', function() {
+        benchTabOptions.hide()
+    })
+}
 
 
 
 // main
+
+// do optimizer ui stuff
 updateMyTheme()
 $('#dark-theme-toggle').on('change', updateMyTheme)
 createCollapsePanels()
 createFieldCollapseBtn()
+doPanels()
+fixCalculationResults()
+createBenchBrowser()
 // fix buttons for opera
 $('[id=clearSets]').removeClass('btn-4xwide')
 $('[id=clearSets]').addClass('btn-4xwide')
 $('button').addClass('btn')
+
+console.log('done')
